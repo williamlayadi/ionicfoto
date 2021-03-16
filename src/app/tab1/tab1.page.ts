573f558a3c68ece5cbe1ac4a052f381658d85b02
William <c14180054@john.petra.ac.id>
2021-03-16 20:32:54 +0700

    last commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../service/foto.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public fotoService:FotoService) {}
+
+  async ngOnInit(){
+    await this.fotoService.loadFoto();
+  }
+  TambahFoto(){
+    this.fotoService.tambahFoto();
+  }
 
 }
