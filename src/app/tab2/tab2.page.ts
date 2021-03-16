943e6b13f14634e68233f6764dcb07552a587e8f
William <c14180054@john.petra.ac.id>
2021-03-16 20:33:06 +0700

    last commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../service/foto.service';
 
 @Component({
   selector: 'app-tab2',
@@ -9,4 +10,23 @@ export class Tab2Page {
 
   constructor() {}
 
+
+  
+  betul : boolean = false;
+  a =0;
+  //b = Math.random()*20
+  b = Math.floor(Math.random()*10+1) 
+
+  text :string;
+  result(){
+    if(this.a == this.b){
+      this.text = "yes"
+      this.betul = true;
+    }
+    else{
+      this.text = "no"
+      this.betul = false;
+    }
+  }
+  
 }
